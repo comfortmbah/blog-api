5597102a5c51d35fc3f18494b64db3e113627203
sylvester <sylvestermbah90@gmail.com>
2026-08-19 08:48:52 -0700

    Update to use global custom error

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 import express from "express";
 import postRoutes from "./routes/postRoutes.js"
+import { errorHandler } from "./middleware/errorHandler.js";
 
 const app = express();
 
 app.use(express.json());
 
 app.use("/api/posts", postRoutes);
+
+app.use(errorHandler);
 
 app.listen(4000, () => {
   console.log("Server running on port 4000");
