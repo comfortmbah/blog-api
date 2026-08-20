269b764ea84ae1edb2d3ba5d519df9d7f7ace2b9
sylvester <sylvestermbah90@gmail.com>
2026-08-20 07:59:09 -0700

    Add create post route

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getPosts, getPost } from "../controllers/postControllers.js";
+import { getPosts, getPost, createPost } from "../controllers/postControllers.js";
 
 
 const router = express.Router();
@@ -7,5 +7,7 @@ const router = express.Router();
 router.get("/", getPosts);
 
 router.get("/:id", getPost);
+
+router.post("/", createPost);
 
 export default router;
