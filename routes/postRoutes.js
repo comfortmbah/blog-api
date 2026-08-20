9030c266b42b1aeb09766085edd0ffcd03b4e448
sylvester <sylvestermbah90@gmail.com>
2026-08-20 13:20:46 -0700

    Add delete post route

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getPosts, getPost, createPost, updatePost, updatePostPartially } from "../controllers/postControllers.js";
+import { getPosts, getPost, createPost, updatePost, updatePostPartially, deletePost } from "../controllers/postControllers.js";
 
 
 const router = express.Router();
@@ -13,5 +13,7 @@ router.post("/", createPost);
 router.put("/:id", updatePost);
 
 router.patch("/:id", updatePostPartially);
+
+router.delete("/:id", deletePost);
 
 export default router;
