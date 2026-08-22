987bc9780a1a875b4acb5704d045c610c64e26bc
sylvester <sylvestermbah90@gmail.com>
2026-08-22 08:00:46 -0700

    Update

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,19 +1,20 @@
 import express from "express";
 import { getPosts, getPost, createPost, updatePost, updatePostPartially, deletePost } from "../controllers/postControllers.js";
+import { asyncHandler } from "../utils/asyncHandler.js"
 
 
 const router = express.Router();
 
-router.get("/", getPosts);
+router.get("/", asyncHandler(getPosts));
 
-router.get("/:id", getPost);
+router.get("/:id", asyncHandler(getPost));
 
-router.post("/", createPost);
+router.post("/", asyncHandler(createPost));
 
-router.put("/:id", updatePost);
+router.put("/:id", asyncHandler(updatePost));
 
-router.patch("/:id", updatePostPartially);
+router.patch("/:id", asyncHandler(updatePostPartially));
 
-router.delete("/:id", deletePost);
+router.delete("/:id", asyncHandler(deletePost));
 
 export default router;
