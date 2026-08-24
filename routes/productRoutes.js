8c7139b6072df669fb6f54e456836946186ae5da
sylvester <sylvestermbah90@gmail.com>
2026-08-24 03:20:22 -0700

    Add update product route

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,12 +1,18 @@
 import express from "express"
-import { getProducts, getProduct, createProduct } from "../controllers/productController.js";
+import { getProducts, getProduct, createProduct, updateProduct } from "../controllers/productController.js";
 import { asyncHandler } from "../utils/asyncHandler.js"
 
 
 const router = express.Router();
 
 router.get("/", asyncHandler(getProducts));
+
 router.get("/:id", asyncHandler(getProduct));
+
 router.post("/", asyncHandler(createProduct));
+
+router.put("/:id", asyncHandler(updateProduct));
+
+
 
 export default router;
