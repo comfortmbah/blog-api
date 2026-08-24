d45a947c1c951c34f7fbd9dcc7b01ab1c3900770
sylvester <sylvestermbah90@gmail.com>
2026-08-24 02:52:41 -0700

    Refactor postController

--- controllers/postControllers.js
+++ controllers/postControllers.js
@@ -23,20 +23,7 @@ export const posts = [
 ];
 
 export const getPosts = (req, res) => {
-  const page = Number(req.query.page) || 1;
-  const limit = Number(req.query.limit) || 10;
-
-  const total = posts.length;
-  const totalPages = Math.ceil(total / limit);
-
-  if (page > totalPages) {
-    throw new AppError("Page not found", 404);
-  }
-
-  const startIndex = (page - 1) * limit;
-  const paginatedPosts = posts.slice(startIndex, startIndex + limit);
-
-  res.json({ page, limit, total, totalPages, posts: paginatedPosts });
+  res.json(posts);
 }
 
 export const getPost = (req, res) => {
