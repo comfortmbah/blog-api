40c3fd869c9a91c0f404b486a2496dad2f2adc94
sylvester <sylvestermbah90@gmail.com>
2026-08-24 02:59:50 -0700

    Add get product route

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,10 +1,11 @@
 import express from "express"
-import { getProducts } from "../controllers/productController.js";
+import { getProducts, getProduct } from "../controllers/productController.js";
 import { asyncHandler } from "../utils/asyncHandler.js"
 
 
 const router = express.Router();
 
 router.get("/", asyncHandler(getProducts));
+router.get("/:id", asyncHandler(getProduct));
 
 export default router;
