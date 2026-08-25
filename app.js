6f0b2930d59c5114dcb23368b64ae5d48861865a
sylvester <sylvestermbah90@gmail.com>
2026-08-25 10:45:54 -0700

    Mount the view routes

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from "express";
 import postRoutes from "./routes/postRoutes.js"
 import productRoutes from "./routes/productRoutes.js"
 import { errorHandler } from "./middleware/errorHandler.js";
+import productViewRoutes from "./routes/productViewRoutes.js"
 
 const app = express();
 
@@ -11,6 +12,8 @@ app.use(express.json());
 
 app.use("/api/posts", postRoutes);
 app.use("/api/products", productRoutes);
+
+app.use("/products", productViewRoutes);
 
 app.use(errorHandler);
 
