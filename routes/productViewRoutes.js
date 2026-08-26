c7b7c7c9d39ecf98aaa6063a30d4cd9870ac004e
sylvester <sylvestermbah90@gmail.com>
2026-08-26 10:48:25 -0700

    Add the POST route for creating product form

--- routes/productViewRoutes.js
+++ routes/productViewRoutes.js
@@ -1,10 +1,12 @@
 import express from "express";
-import { showProducts, showCreateProductForm } from "../controllers/productViewController.js";
+import { showProducts, showCreateProductForm, createProductFromForm } from "../controllers/productViewController.js";
 
 const router = express.Router();
 
 router.get("/", showProducts);
 
 router.get("/new", showCreateProductForm);
+
+router.post("/", createProductFromForm);
 
 export default router;
