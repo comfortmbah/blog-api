2c564eb8061d2ddb9b4af31c4a2fd28598246b7d
sylvester <sylvestermbah90@gmail.com>
2026-08-26 10:46:05 -0700

    Add form controller for creating products

--- controllers/productViewController.js
+++ controllers/productViewController.js
@@ -9,3 +9,20 @@ export const showProducts = (req, res) => {
 export const showCreateProductForm = (req, res) => {
   res.render("products/new");
 }
+
+export const createProductFromForm = (req, res) => {
+  console.log(req.body);
+
+  const { name, price, category } = req.body;
+
+  const newProduct = {
+    id: products.length + 1,
+    name,
+    price: Number(price),
+    category
+  };
+
+  products.push(newProduct);
+
+  res.redirect("/products");
+}
