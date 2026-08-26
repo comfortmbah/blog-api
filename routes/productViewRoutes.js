3a4e1c3190e51ba5736767eff4957d43c93f94e4
sylvester <sylvestermbah90@gmail.com>
2026-08-26 10:29:51 -0700

    Add route to create product form

--- routes/productViewRoutes.js
+++ routes/productViewRoutes.js
@@ -1,8 +1,10 @@
 import express from "express";
-import { showProducts } from "../controllers/productViewController.js";
+import { showProducts, showCreateProductForm } from "../controllers/productViewController.js";
 
 const router = express.Router();
 
 router.get("/", showProducts);
+
+router.get("/new", showCreateProductForm);
 
 export default router;
