317eb78d1a10f0c1acaa1ec44fdc7ff56ab4987e
sylvester <sylvestermbah90@gmail.com>
2026-08-27 05:49:38 -0700

    Add route for editing a product

--- routes/productViewRoutes.js
+++ routes/productViewRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { showProducts, showCreateProductForm, createProductFromForm } from "../controllers/productViewController.js";
+import { showProducts, showCreateProductForm, createProductFromForm, showEditProductForm } from "../controllers/productViewController.js";
 import { validateProductForm } from "../middleware/validateProductForm.js";
 
 const router = express.Router();
@@ -7,6 +7,8 @@ const router = express.Router();
 router.get("/", showProducts);
 
 router.get("/new", showCreateProductForm);
+
+router.get("/:id/edit", showEditProductForm);
 
 router.post("/", validateProductForm, createProductFromForm);
 
