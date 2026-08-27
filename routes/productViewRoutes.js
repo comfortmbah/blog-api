6ed534b0a11b3f655d03b854a43e54cf715d9747
sylvester <sylvestermbah90@gmail.com>
2026-08-27 05:09:25 -0700

    Add product form validation middleware

--- routes/productViewRoutes.js
+++ routes/productViewRoutes.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { showProducts, showCreateProductForm, createProductFromForm } from "../controllers/productViewController.js";
+import { validateProductForm } from "../middleware/validateProductForm.js";
 
 const router = express.Router();
 
@@ -7,6 +8,6 @@ router.get("/", showProducts);
 
 router.get("/new", showCreateProductForm);
 
-router.post("/", createProductFromForm);
+router.post("/", validateProductForm, createProductFromForm);
 
-export default router;
+export default router;  
