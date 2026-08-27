c58d776564bf5f5e34ccce1a7155b4b2ca51f860
sylvester <sylvestermbah90@gmail.com>
2026-08-27 06:10:00 -0700

    Add update form route

--- routes/productViewRoutes.js
+++ routes/productViewRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { showProducts, showCreateProductForm, createProductFromForm, showEditProductForm } from "../controllers/productViewController.js";
+import { showProducts, showCreateProductForm, createProductFromForm, showEditProductForm, updateProductFromForm } from "../controllers/productViewController.js";
 import { validateProductForm } from "../middleware/validateProductForm.js";
 
 const router = express.Router();
@@ -11,5 +11,7 @@ router.get("/new", showCreateProductForm);
 router.get("/:id/edit", showEditProductForm);
 
 router.post("/", validateProductForm, createProductFromForm);
+
+router.post("/:id/edit", validateProductForm, updateProductFromForm);
 
 export default router;  
